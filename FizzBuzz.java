9f8351896e61a8b25aef9920b7243df5c4e99f1d
jjivy12 <jordan.ivy12@gmail.com>
2020-06-01 11:00:35 -0400

    final commit with 2 passed test

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class FizzBuzz {
-    
+    public String execute(int[] numbers) {
+        // Creating a sequence from each member of our array, passing it as a parameter
+        // of our processNumber() and then collect the output and join them together
+        // seperated by a comma
+        return Arrays.stream(numbers).mapToObj(this::processNumber).collect(Collectors.joining(", "));
+    }
+
+    public String processNumber(int number) {
+        if (isMultipleOf3(number) && isMultipleOf5(number)) {
+            return "Fizz Buzz";
+        } else if (isMultipleOf3(number)) { // if number is divisble by 3, remainder will be 0
+            return "Fizz";
+        } else if (isMultipleOf5(number)) { // if number if divisible by 5, remainder will be 0
+            return "Buzz";
+        } else {
+            return Integer.toString(number);
+        }
+    }
+
+    private boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
+    }
 }
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,3 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FizzBuzzTest {
-    
+    @Test
+    public void shouldExecute() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.execute(new int[] { 1 }));
+        assertEquals("1, 2, Fizz, Buzz, Fizz, Buzz, Fizz Buzz, Fizz Buzz",
+                fizzBuzz.execute(new int[] { 1, 2, 3, 5, 6, 10, 15, 30 }));
+    }
+
+    @Test
+    public void shouldProcessInput() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.processNumber(1));
+        assertEquals("2", fizzBuzz.processNumber(2));
+        assertEquals("Fizz", fizzBuzz.processNumber(3));
+        assertEquals("Buzz", fizzBuzz.processNumber(5));
+        assertEquals("Fizz", fizzBuzz.processNumber(6));
+        assertEquals("Buzz", fizzBuzz.processNumber(10));
+        assertEquals("Fizz Buzz", fizzBuzz.processNumber(15));
+    }
 }
